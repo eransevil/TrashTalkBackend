9b148d208176690a076d011724e1937dd74093f1
eran Sevil <eransev@clalit.org.il>
2024-01-28 11:10:30 +0200

    adding first user

---
+++ routes/users.js
@@ -0,0 +1,86 @@
+const express = require("express");
+
+// userRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /user.
+const userRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+// This section will help you get a list of all the users.
+userRoutes.route("/user").get(async (req, response) => {
+  let db_connect = dbo.getDb();
+
+  db_connect
+    .collection("users")
+    .find({})
+    .toArray()
+    .then((data) => {
+      console.log(data);
+      response.json(data);
+    });
+});
+// This section will help you get a single user by id
+userRoutes.route("/user/:id").get((req, res) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: new ObjectId(req.params.id) };
+  db_connect
+    .collection("users")
+    .findOne(myquery)
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+// This section will help you create a new user.
+userRoutes.route("/user/add").post(async (req, response) => {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    fullname: req.body.fullname || 'testtest',
+    isAdmin: req.body.isAdmin || 'false',
+    username: req.body.username || 'test',
+    password: req.body.password || 'a123456z',
+  };
+  db_connect.collection("users").insertOne(myobj) .then((data) => {
+    console.log(data);
+    response.json(data);
+  });
+});
+
+// This section will help you update a user by id.
+userRoutes.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    },
+  };
+  db_connect
+    .collection("users")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete a user
+userRoutes.route("/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("users").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = userRoutes;
