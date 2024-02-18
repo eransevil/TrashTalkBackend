7dc1a36ec77fcc19089409e84c6ca4c3ca345fc2
eran Sevil <eransevil2@gmail.com>
2024-02-18 16:15:38 +0200

    f

--- api/auth/authController.js
+++ api/auth/authController.js
@@ -1,43 +1,50 @@
-const authService = require('./authService.js')
-const logger = require('../../services/loggerService')
+const authService = require("./authService.js");
+const logger = require("../../services/loggerService");
 
 async function login(req, res) {
-    const { username, password } = req.body
-    try {
-        const user = await authService.login(username, password)
-        req.session.user = user
-        res.json(user)
-    } catch (err) {
-        logger.error('Failed to Login ' + err)
-        res.status(401).send({ err: 'Failed to Login' })
-    }
+  const { username, password } = req.body;
+  try {
+    const user = await authService.login(username, password);
+    req.session.user = user;
+    res.json(user);
+  } catch (err) {
+    logger.error("Failed to Login " + err);
+    res.status(401).send({ err: "nvalid username or password" });
+  }
 }
 
 async function signup(req, res) {
-    try {
-        const { username, password, fullname, imgUrl } = req.body
-        const account = await authService.signup(username, password, fullname, imgUrl)
-        logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
-        const user = await authService.login(username, password, imgUrl)
-        req.session.user = user
-        res.json(user)
-    } catch (err) {
-        logger.error('Failed to signup ' + err)
-        res.status(500).send({ err: 'Failed to signup' })
-    }
+  try {
+    const { username, password, fullname, imgUrl } = req.body;
+    const account = await authService.signup(
+      username,
+      password,
+      fullname,
+      imgUrl
+    );
+    logger.debug(
+      `auth.route - new account created: ` + JSON.stringify(account)
+    );
+    const user = await authService.login(username, password, imgUrl);
+    req.session.user = user;
+    res.json(user);
+  } catch (err) {
+    logger.error("Failed to signup " + err);
+    res.status(500).send({ err: "Failed to signup" });
+  }
 }
 
 async function logout(req, res) {
-    try {
-        req.session.destroy()
-        res.send({ msg: 'Logged out successfully' })
-    } catch (err) {
-        res.status(500).send({ err: 'Failed to logout' })
-    }
+  try {
+    req.session.destroy();
+    res.send({ msg: "Logged out successfully" });
+  } catch (err) {
+    res.status(500).send({ err: "Failed to logout" });
+  }
 }
 
 module.exports = {
-    login,
-    signup,
-    logout
-}
+  login,
+  signup,
+  logout,
+};
